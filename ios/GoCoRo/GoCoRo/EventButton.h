bff5509bc53203b4cc7b489abdc2fd8945044f0f
tfeng <tang.feng@wcare.cn>
2017-05-05 21:37:50 +0800

    web share image

---
+++ ios/GoCoRo/GoCoRo/EventButton.h
@@ -0,0 +1,13 @@
+//
+//  EventButton.h
+//  GoCoRo
+//
+//  Created by ttonway on 2017/5/5.
+//  Copyright © 2017年 wcare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventButton : UIButton
+
+@end
---
+++ ios/GoCoRo/GoCoRo/EventButton.m
@@ -0,0 +1,21 @@
+//
+//  EventButton.m
+//  GoCoRo
+//
+//  Created by ttonway on 2017/5/5.
+//  Copyright © 2017年 wcare. All rights reserved.
+//
+
+#import "EventButton.h"
+
+@implementation EventButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
